feb84e167b8a72f04c98e9a740bb289f734b77b5
Dante Dobson-Davies <144271311+DantesPhoenix@users.noreply.github.com>
2025-02-11 18:29:52 +0000

    Update main.rs
    added hashing function, will add verification next

--- main.rs
+++ main.rs
@@ -1,10 +1,15 @@
 use std::io::{self, Write}; // Import Write for flushing
+use argon2::{Argon2, PasswordHasher, Params, password_hash::{SaltString, rand_core::OsRng}}; // import argon2 for hashing, verification and salting
 
 fn main() {
-    let master_password = get_pass(); // Store the returned password
-    println!("You entered: {}", master_password);
+    // Store the returned password
+    let master_password = get_pass(); 
+
+    let hashed_brown = hashing_password(&master_password);
+    println!("Your hash brown is ready!: {}", hashed_brown)
 }
 
+// Defining function to get the master password
 fn get_pass() -> String {
     let mut password = String::new();
 
@@ -17,4 +22,21 @@ fn get_pass() -> String {
 
     // Trim and return the password
     password.trim().to_string()
+}
+
+//defining function that will hash and return the master password
+fn hashing_password(master_password: &str) -> String {
+    //generating salt using OS random generator
+    let salt = SaltString::generate(&mut OsRng);
+
+    // Setting parameters for hashing: 64mb memory, 2 core and 4 threads used
+    let params = Params::new(65536, 6, 2, None).expect("Invalid Argon2 params"); 
+    let argon2 = Argon2::new(argon2::Algorithm::Argon2id, argon2::Version::V0x13, params);
+
+    let hashed_password = argon2.hash_password(master_password.as_bytes(), &salt)
+    .expect("Failed to hash password")
+    .to_string();
+
+    //return hashed password
+    hashed_password
 }
